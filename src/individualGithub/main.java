f30cc741a73f952dcb7ca2a975844d22bc374149
JoshBasham <joshbasham34@gmail.com>
2021-09-16 16:41:41 -0700

    Added method printSortedArray

--- src/individualGithub/main.java
+++ src/individualGithub/main.java
@@ -1,10 +1,25 @@
 package individualGithub;
 
+import java.util.Arrays;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] arr = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,12,11,10,19};
+		
+		printSortedArray(arr);
+		
 	}
+
+
+public static void printSortedArray(int[] arr) {
+	
+	int[] arrCopy = Arrays.copyOf(arr, arr.length);
+	Arrays.sort(arrCopy);
+	
+	System.out.println(Arrays.toString(arrCopy));
+	
+}
 
 }
